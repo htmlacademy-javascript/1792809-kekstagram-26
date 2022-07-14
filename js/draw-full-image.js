101268280df373bcd8a5fc1360f359ffa6e9c127
Олег <exdns@mail.ru>
2022-07-14 23:37:02 +0300

    Исправляет ошибки ДЗ № 7.15(2)

--- js/draw-full-image.js
+++ js/draw-full-image.js
@@ -3,7 +3,7 @@ import {posts} from './data.js';
 const fullImg = document.querySelector('.big-picture');                         //секция для просмотра больших фоток
 const imgContainer = document.querySelectorAll('.picture');                     //контейнер всех фоток
 
-const bigPhoto = fullImg.querySelector('.big-picture__img');                    //фото
+const bigPhoto = fullImg.querySelector('.big-picture__img img');                    //фото
 const bigPhotoLikes = fullImg.querySelector('.likes-count');                   //лайки
 const bigPhotoCommentsCount = fullImg.querySelector('.comments-count');        //кол-во комментариев
 const bigPhotoComments = fullImg.querySelector('.social__comments');           //комментарии к фото
@@ -25,6 +25,7 @@ const showWindow = () => {                                                      
 
 const closeWindow = () => {                                                    //убираем его
   document.body.classList.remove('modal-open');
+  fullImg.classList.add('hidden');
 };
 
 const showFullImg = (Index) => {                                                //заполняем данными по конкретной фотографии
@@ -37,4 +38,42 @@ const showFullImg = (Index) => {                                                
 
 const templateСomments = (Index) => {                                             // функция для создания social__comments
   const commentsContainerFragment = document.createDocumentFragment();           //  шаблон для social__comments
+
+  posts[Index].comments.forEach(({avatar, name, message}) => {
+    const comment = document.createElement('li');
+    const commentAvatar = document.createElement('img');
+    const commentText = document.createElement('p');
+
+    comment.classList.add('social__comment');
+    commentAvatar.classList.add('social__picture');
+    commentAvatar.src = avatar;
+    commentAvatar.alt = name;
+    commentAvatar.width = 35;
+    commentAvatar.height = 35;
+    comment.append(commentAvatar);
+    commentText.classList.add('social__text');
+    commentText.textContent = message;
+    comment.append(commentText);
+    commentsContainerFragment.append(comment);
+  });
+  bigPhotoComments.textContent = '';
+  bigPhotoComments.append(commentsContainerFragment);
 };
+
+imgContainer.forEach((photo, index) => {
+  photo.addEventListener('click', showWindow );
+  photo.addEventListener('click', hiddenElements);
+  photo.addEventListener('click', showFullImg.bind(null, index));
+  photo.addEventListener('click', templateСomments.bind(null, index));
+});
+
+cancelButton.addEventListener('click', () => {
+  closeWindow();
+});
+
+document.addEventListener('keydown', (event) => {
+  if (event.code === 'Escape') {
+    closeWindow();
+  }
+});
+
